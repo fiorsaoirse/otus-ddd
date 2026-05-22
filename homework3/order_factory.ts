5554f69fbee2c33582cea2dc361948a2cd308731
Mariia Kovsharova <40024492+fiorsaoirse@users.noreply.github.com>
2026-05-22 16:16:16 +0200

    Update order_factory.ts

--- homework3/order_factory.ts
+++ homework3/order_factory.ts
@@ -8,7 +8,7 @@ export interface IOrderFactory {
 
 export class OrderFactory implements IOrderFactory {
   create(id: ID, customer: Customer): Order {
-    // Бизнес-логика в фабрике (чтобы она была анемичной, иначе в ней нет особого смысла)
+    // Бизнес-логика в фабрике (чтобы она не была анемичной, иначе в ней нет особого смысла)
     const maxCount = customer.level === "vip" ? 20 : 10;
     return Order.create(id, customer.id, maxCount);
   }
